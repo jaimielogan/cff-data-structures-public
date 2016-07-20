ed2c6929d20c4aeabdb97bf2f1d507f202c2521d
jaimielogan <jaimielogan@gmail.com>
2016-07-19 19:03:44 -0600

    Store1 and Store2 Solutions

--- store1/exercise.js
+++ store1/exercise.js
@@ -4,33 +4,62 @@ var store1 = require('./store1-data.js');
 
 // How would you access the value '4.63' from store1?
 
+console.log(store1["2015-01-08"][0][1]);
+
 
 // How would you access how many 'Mint Wafers' were sold on January 7th?
 
+console.log(store1["2015-01-07"][3][0]);
 
 // Produce an array of the date keys in store1's data.
-
+var dateKeys = [];
+for (var key in store1){
+  dateKeys.push(key);
+}
+console.log(DateKeys);
 
 // *********** LOOPING OVER DATA *********** \\
 
 
 // Create a loop to read which candies were sold by store1 on Jan 8. After simply outputting the data, try creating an array that contains the candy names.
-
+var candy = [];
+for (i=0; i<store1["2015-01-08"].length; i++){
+	candy.push(store1["2015-01-08"][i][0]);
+}
+console.log(candy);
 
 // Create a loop to count the total number of candies sold on Jan 10 at store1. Where do you have to initialize the counter variable? Why?
-
+var count = 0;
+for(i=0; i<store1["2015-01-10"].length; i++){
+	count += store1["2015-01-10"][i][2];
+}
+console.log(count);
 
 // Use `Object.keys()` to get an array of the dates that candies were sold at store1.
-
+console.log(Object.keys(store1));
 
 // Iterate over the generated array of dates. Use each date to console.log the specific sale data for the day from store1.
-
+for (i=0; i<Object.keys(store1).length; i++){
+	console.log(store1[Object.keys(store1)[i]]);
+}
 
 // Use a loop to calculate the total number of candies sold at store1.
+var count = 0;
+for (j=0; j<Object.keys(store1).length; j++){
+	for (i=0; i<store1[Object.keys(store1)[j]].length; i++){
+		count += store1[Object.keys(store1)[j]][i][2];
+	}
+}
+console.log(count);
 
 // In the previous exercise, where did you have to initialize the counter variable? Why?
-
+// There were two counter variables to loop through the date arrays and then the candy arrays for each date
 
 // *********** CHALLENGE *********** \\
 
 // Create an array of the candies sold by store1 on January 10th.
+var candy = [];
+for (i=0; i<store1["2015-01-10"].length; i++){
+	candy.push(store1["2015-01-10"][i][0]);
+}
+console.log(candy);
--- store2/exercise.js
+++ store2/exercise.js
@@ -3,27 +3,88 @@ var store2 = require('./store2-data.js');
 /////////// ACCESSING DATA ///////////
 
 // Set the sale dates for 'Banana Bunches' from store2 to a variable.
-
+var BananaBunchesSaleDates = store2["sale dates"]["Banana Bunches"];
+console.log(BananaBunchesSaleDates);
 
 // What built-in array property would you use to determine how many sale dates there for store2's 'Banana Bunches'?
-
+console.log(saleDates.length);
 
 /////////// UPDATING DATA ///////////
 
 // Set a variable to equal the prices of the 'Mint Wafers' in store2. Then reassign it to a new value. When you update your new variable, does the price in the store2 data change? Why or why not?
-
+var mintWafers = store2["inventory prices"]["Mint Wafers"];
+console.log(mintWafers);
+mintWafers = 1.50;
+console.log(mintWafers);
+console.log(store2["inventory prices"]["Mint Wafers"]);
+// No the price within the object does not change because it is inmutable
 
 // Set a variable equal to the Peanut Butter Buttered Peanuts sale dates in store2. Use pop to remove one of the sale dates for store2's Caramel Twists.
+var PBPeanutsSalesDates = store2["sale dates"]["Peanut Butter Buttered Peanuts"];
+console.log(PBPeanutsSalesDates);
+
+store2["sale dates"]["Caramel Twists"].pop();
+console.log(store2["sale dates"]["Caramel Twists"]);
 
 /////////// LOOPING OVER DATA ///////////
 
 // Iterate over the store2 sale dates for Caramel Twists. Use this to create an object containing dates as the key and the value as the quantity sold on that day.
+var salesDates = [];
+var caramelTwists = {};
+for (var key of store2["sale dates"]["Caramel Twists"]){
+	salesDates.push(key);
+}
 
+salesDates.forEach(function(x){
+	if(!caramelTwists[x]){
+		caramelTwists[x] = 0;
+	}
+	caramelTwists[x] += 1;
+});
+
+console.log(caramelTwists);
 
 // Iterate over store2's sale dates data to find which day had the most total number of sales. How did you calculate this?
+var salesDates_all = [];
+for (var key in store2["sale dates"]){
+	for (var i of store2["sale dates"][key]){
+		salesDates_all.push(i);
+	}
+}
+console.log(salesDates_all);
 
+var totalSales = {};
+salesDates_all.forEach(function(x){
+	if(!totalSales[x]){
+		totalSales[x] = 0;
+	}
+	totalSales[x] += 1;
+});
+
+console.log(totalSales);
 
 /////////// CHALLENGE ///////////
 
 // Create an object containing the amounts that store2 made by selling each type of candy.
+// Pull the sales data objects for each candy
+var candySales = store2["sale dates"];
+console.log(candySales);
+console.log("-------");
 
+//Create an empty object to populate teh candies and total sales volume
+var candySaleDates = {};
+
+//loop through the keys in candySales
+for(var key in candySales){
+	//within the array, loop through the dates
+	for (var i = 0; i<candySales[key].length; i++){
+		// if the date is not within our candySaleDate object...
+		if (!candySaleDates[key]){
+			//create it and populate it with a value of 0
+		candySaleDates[key] = 0;
+		}
+		// Count up the sales volume by 1
+		candySaleDates[key] += 1;
+	}
+}
+console.log(candySaleDates);
